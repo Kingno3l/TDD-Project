5df98f11142b578705a01b9931a9ef2d887ba623
Burhan Uddin <61288243+BHObserver@users.noreply.github.com>
2023-12-02 18:48:42 +0600

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -10,11 +10,5 @@ def reverse(word)
     word.reverse
   end
 
-  def fizzbuzz(num)
-    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
-    return 'fizz' if (num % 3).zero?
-    return 'buzz' if (num % 5).zero?
-
-    num.to_s
-  end
+  
 end
